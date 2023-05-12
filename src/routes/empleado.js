c91b66f8904f7962f25d81ed04a9a5c6a82ff4cf
Mike2611 <mpuentes2611@gmail.com>
2023-05-12 17:05:36 -0600

    Add routes for empleado

--- src/routes/empleado.js
+++ src/routes/empleado.js
@@ -1,2 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../database');
+
+
+//GET todos los empleados
+router.get('/', (req, res) => {
+    let sql = 'SELECT * FROM tbl_empleado';
+    db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.json(results);
+    })
+});
+
+//Get un empleado por id
+router.get('/:id', (req, res) => {
+    let sql = `SELECT * FROM tbl_empleado WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    })
+})
+
+//Eliminar a un empleado
+router.delete('/:id', (req, res) => {
+    let sql = `DELETE FROM tbl_empleado WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    })
+})
+
+//Añadir a un empleado
+router.get('/', (req,res) => {
+    let newEmpleado = req.body;
+    let sql = 'INSERT INTO tbl_empleado SET ?';
+    db.query(sql, newEmpleado, (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    })
+});
+
+//Editar a un empleado
+router.put('/:id', (req, res) => {
+    let updateEmpleado = req.body;
+    let sql = 'UPDATE tbl_empleado SET ? WHERE id = ?';
+    db.query(sql, [updateEmpleado, req.params.id], (err, result) => {
+        if(err) throw err;
+        res.json(result);
+    })
+});
