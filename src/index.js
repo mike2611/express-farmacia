9f4f3de6b02d89b56bab3fb14467ae93cc27688b
Mike2611 <mpuentes2611@gmail.com>
2023-05-11 22:08:54 -0600

    Setup configuration for connection to DB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import express from 'express';
+const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
+app.use(express.json());
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+//Connection to de DB
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'contrasena',
+    database: 'bd_farmacia'
 });
+
+db.connect((err) => {
+    if(err) {
+        throw err;
+    }
+    console.log('Connection established to MYSQL....')
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
