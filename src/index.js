ed0324f50326fc2b2b2fc453e81f006897a2842a
Mike2611 <mpuentes2611@gmail.com>
2023-05-11 22:24:09 -0600

    Add datbase connection configuration file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 const express = require('express');
-const mysql = require('mysql2');
 
 const app = express();
 app.use(express.json());
 
 const port = 3000;
-
-//Connection to de DB
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'contrasena',
-    database: 'bd_farmacia'
-});
-
-db.connect((err) => {
-    if(err) {
-        throw err;
-    }
-    console.log('Connection established to MYSQL....')
-})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
