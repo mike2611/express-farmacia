7e670b2b39c27ab0385083e237d5e763f4271e0c
Mike2611 <mpuentes2611@gmail.com>
2023-05-13 18:58:29 -0600

    Change route name perfil -> perfiles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/empleados', routes.empleado);
-app.use('/perfil', routes.perfil);
+app.use('/perfiles', routes.perfil);
 
 const port = 3000;
 
