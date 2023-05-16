8a7f2773e94cb97933392b2a335cf026d4220e00
Mike2611 <mpuentes2611@gmail.com>
2023-05-16 16:32:23 -0600

    Add inventario route to routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const empleado = require('./empleado');
 const perfil = require('./perfil');
+const inventario = require('./inventario');
 
 module.exports = {
     empleado,
     perfil,
+    inventario
 };
